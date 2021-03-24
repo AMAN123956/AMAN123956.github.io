d634b7b7a4bc61058c6a133edbc30bf696c3c951
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-03-24 20:21:54 +0530

    Added dark and bright mode logic

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,7 +1,6 @@
 
 
 let skillButton = document.querySelectorAll(".skill-button");
-
 let skillHeading = document.querySelector(".skill-heading");
 let skillBox = document.querySelectorAll(".skill-box");
 console.log(skillHeading.length);
@@ -21,3 +20,48 @@ for (let i = 0; i < skillButton.length; i++){
        
     })
 }
+
+
+
+/* Floating Button Logic Code */
+const floatingOne = document.getElementById("float-one");
+const navbar = document.getElementById("navbar");
+const body = document.getElementById("bad");
+const educationHeader = document.getElementById("education-header");
+const projectTitle = document.querySelector(".project-title");
+const skillHeader = document.getElementById("skill-header");
+const accomplishment = document.getElementById("a-header");
+const contact = document.querySelector(".reach-me");
+const footer = document.getElementById("footer");
+const moon = document.getElementById("moon");
+const sun = document.getElementById("sun");
+
+// Dark Mode
+moon.addEventListener("click", () => {
+    moon.style.display = "none";
+    sun.style.color = "yellow";
+    sun.style.display = "block";
+    educationHeader.style.background = "linear-gradient(to top left,#1F1B24 50%,black)";
+    projectTitle.style.background = "linear-gradient(to top left,#1F1B24 50%,black)";
+    skillHeader.style.background = "linear-gradient(to top left,#1F1B24 50%,black)";
+    navbar.style.background="linear-gradient(to top left,#5e81cf 50%, #5e81cf)";
+    accomplishment.style.background = "linear-gradient(to top left,#1F1B24 50%,black)";
+    contact.style.background = "linear-gradient(to top left,#1F1B24 50%,black)";
+    footer.style.background = "linear-gradient(to top left,#1F1B24 50%,black)";
+    footer.style.opacity = "0.8";
+})
+
+// Bright Mode
+sun.addEventListener("click", () => {
+    sun.style.display = "none";
+    moon.style.display = "block";
+    educationHeader.style= "background: url(img/b2.png);background-color: #1B9CFC;background-repeat: repeat-y;";
+    projectTitle.style= "background: url(img/b2.png);background-color: #1B9CFC;background-repeat: repeat-y;";
+    projectTitle.style="color:white;"
+    skillHeader.style="background: url(img/b2.png);background-color: #1B9CFC;background-repeat: repeat-y;";
+    navbar.style="background: url(img/b2.png);background-color: #1B9CFC;background-repeat: repeat-y;";
+    accomplishment.style="background: url(img/b2.png);background-color: #1B9CFC;background-repeat: repeat-y;";
+    contact.style="background: url(img/b2.png);background-color: #1B9CFC;background-repeat: repeat-y;";
+    footer.style="background: url(img/b2.png);background-color: #1B9CFC;background-repeat: repeat-y;";
+    footer.style.opacity = "1";
+})
